0e4ad43f3f97590242e77e8daa7811771d60adec
Saad Rasheed <57128684+SaadRasheed-exe@users.noreply.github.com>
2019-11-20 15:55:43 +0500

    Add files via upload

---
+++ String Manipulation 2.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Removant As Char
+        Dim LineString As String
+        Dim Output As String = ""
+        Dim Count As Integer
+
+        Console.WriteLine("Input the character to remove.")
+        Removant = Console.ReadLine
+
+        Console.WriteLine("Input the string to remove the character from.")
+        LineString = Console.ReadLine
+
+        For Count = 1 To Len(LineString)
+
+            If Mid(LineString, Count, 1) = Removant Then
+            Else
+                Output &= Mid(LineString, Count, 1)
+            End If
+
+        Next
+
+        Console.WriteLine(Output)
+
+        Console.ReadKey()
+    End Sub
+
+End Module
