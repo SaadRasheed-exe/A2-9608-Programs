4b974615d962db2e3d9eebbc41f310f715a102c2
Saad Rasheed <57128684+SaadRasheed-exe@users.noreply.github.com>
2019-11-20 15:55:20 +0500

    Add files via upload

---
+++ String Manipulation 1.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim String1 As String
+        Dim String2 As String
+        Dim Output As String = ""
+
+        Console.WriteLine("Enter the first string.")
+        String1 = Console.ReadLine
+        Console.WriteLine("Enter the second string.")
+        String2 = Console.ReadLine
+
+        Dim m As Integer
+        m = InStr(String1, " ")
+        Output &= Left(String1, m - 1)
+
+        Dim check As String = ""
+        m = Len(String2)
+
+        Do Until check = " "
+            check = Mid(String2, m, 1)
+            m -= 1
+        Loop
+
+        Output &= Right(String2, Len(String2) - m)
+        Console.WriteLine(Output & ".")
+
+        Console.ReadKey()
+    End Sub
+
+End Module
